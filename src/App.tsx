76b9d4a72bf6ee9d1dd66b75b18e90444f73b295
lya-melnik <ilay.melnik.kr@gmail.com>
2022-10-31 10:30:34 +0200

    les 11 start grid

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,23 @@ import Technologies from "./Technologies";
 
 const App = () => {
     return <div className="App">
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img alt='logo' src='https://cdn.pixabay.com/photo/2016/11/25/18/24/cat-1858951_960_720.png' height='62'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a href='#'>Profile</a>
+                </div>
+                <div>
+                    <a href='#'>Message</a>
+                </div>
+            </nav>
+
+            <div className='content'></div>
+        </div>
+
+
         <Header/>
         <Technologies/>
     </div>;
