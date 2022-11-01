23d4fa4991046adc1f0da289bd7ab29799176d8c
lya-melnik <ilay.melnik.kr@gmail.com>
2022-11-01 04:55:18 +0200

    les 12 Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header";
-import Technologies from "./Technologies";
+import Header from "./components/Header";
+import {Navbur} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 const App = () => {
     return <div className="App">
         <div className='app-wrapper'>
-            <header className='header'>
-                <img alt='logo' src='https://cdn.pixabay.com/photo/2016/11/25/18/24/cat-1858951_960_720.png' height='62'/>
-            </header>
-            <nav className='nav'>
-                <div>
-                    <a href='#'>Profile</a>
-                </div>
-                <div>
-                    <a href='#'>Message</a>
-                </div>
-            </nav>
 
-            <div className='content'></div>
+            <Header/>
+            <Navbur/>
+            <Profile/>
+
+
         </div>
+    </div>
 
-
-        <Header/>
-        <Technologies/>
-    </div>;
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Header = () => {
+    return (
+
+            <header className='header'>
+                <img alt='logo' src='https://cdn.pixabay.com/photo/2016/11/25/18/24/cat-1858951_960_720.png'/>
+
+            </header>
+
+
+
+    )
+}
+
+// const Header = () => {
+//     return (
+//         <div >
+//             <a href="#">Home </a>
+//             <a href="">News </a>
+//             <a href="">Feed </a>
+//             <a href="">Messages </a>
+//         </div>
+//     )
+// }
+
+
+export default Header;
