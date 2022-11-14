0318eb948f2e763fe90adb3677a82a65dcf0f36c
lya-melnik <ilay.melnik.kr@gmail.com>
2022-11-13 20:42:24 -0800

    less 21, build page Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,16 @@ import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+           <div className={s.dialogsItem}>
+               <div className={s.dialog + ' ' + s.active}>Nina</div>
+               <div className={s.dialog}>Vladimr</div>
+           </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>Hello everyone !</div>
+                <div className={s.message}>How are you ?</div>
+            </div>
         </div>
     );
 };
