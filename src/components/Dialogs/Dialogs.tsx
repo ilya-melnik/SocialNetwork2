d5a79eff1817b793e8f62a2e3da840d408364206
lya-melnik <ilay.melnik.kr@gmail.com>
2023-01-02 14:33:14 -0800

    less 31 ref, (VirtualDom). add message from textarea by ref +  local useState (this realization not correct)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {RefObject, useState} from 'react';
 import s from './Dialogs.module.css'
 import {DialogItem} from "./DialogsItem/DialogItem";
 import {Message} from "./Message/Message";
@@ -12,15 +12,26 @@ export const Dialogs = (props: DialogsType) => {
     let dialogsElements = props.state.dialogs.map(el => <DialogItem name={el.name} id={el.id} img={el.img}/>)
     let messagesElements = props.state.messages.map(m => <Message id={m.id} message={m.message}/>)
 
+
+    const newPostElement = React.createRef<HTMLTextAreaElement>();
+    let [ text, valueState] = useState<string | undefined>('')
+    const addMessage = () =>  valueState(newPostElement.current?.value)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
                 {dialogsElements}
+
+
             </div>
 
             <div className={s.messages}>
                 {messagesElements}
+                <div>{text}</div>
+                <textarea ref={newPostElement}></textarea>
+                <button onClick={addMessage}>addMessage</button>
             </div>
+
         </div>
     );
 };
