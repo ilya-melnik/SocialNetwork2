f99f2d92030ab3056aaf3caf781647298ee453d3
lya-melnik <ilay.melnik.kr@gmail.com>
2023-01-02 14:10:00 -0800

    less 31 ref, (VirtualDom). get value of textarea by ref

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {RefObject} from "react";
 import {Post} from "./Post/Post";
 import s from './MyPosts.module.css'
 import {PostType, ProfilePageType} from "../../../redux/state";
@@ -11,13 +11,19 @@ type MyPostsType = {
 export const MyPost = (props: MyPostsType) => {
 
     let postsElements = props.posts.posts.map(p => <Post id={p.id} message={p.message} likesCount={p.likesCount}/>)
+    const newPostElement:RefObject<HTMLTextAreaElement> = React.createRef();
+
+    const addText = () => {
+        let textValue = newPostElement.current?.value
+        alert(textValue)
+    }
     return (
         <div className={s.postBlock}>
             <h3>My post</h3>
             <div>
-                <div><textarea></textarea></div>
+                <div><textarea ref={newPostElement}></textarea></div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addText}>Add post</button>
                 </div>
 
 
