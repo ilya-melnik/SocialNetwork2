038bffc013363e61601d120dd700c6fe75e05e7c
lya-melnik <ilay.melnik.kr@gmail.com>
2023-05-13 22:54:47 -0700

    less 34 is refactored

---
+++ src/render.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import {addPost, updateNewPostText} from "./redux/state";
+
+
+
+export let rerenderEntireTree = (state:any) =>{
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        </BrowserRouter>, document.getElementById('root')
+    );
+}
+
+
+
+
+
+// export let rerenderEntireTree = () => {
+//     const root = ReactDOM.createRoot(document.getElementById('root'));
+//     root.render(
+//
+//         <React.StrictMode>
+//             <App state={state} addPost={addPost} />
+//         </React.StrictMode>);
